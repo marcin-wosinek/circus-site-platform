6d662a6627812fde4f1b717d708a498ff11ea13a
Marcin Wosinek <marcin.wosinek@gmail.com>
2026-08-12 13:32:32 +0200

    Add legal links to footer in lamutable.es

--- sites/lamutable.es/lamutable/patterns/footer.php
+++ sites/lamutable.es/lamutable/patterns/footer.php
@@ -30,6 +30,9 @@
 				<!-- wp:navigation-link {"label":"<?php esc_attr_e( 'Quiénes somos', 'lamutable' ); ?>","url":"<?php echo esc_url( home_url( '/quienes-somos/' ) ); ?>"} /-->
 				<!-- wp:navigation-link {"label":"<?php esc_attr_e( 'Colaboraciones', 'lamutable' ); ?>","url":"<?php echo esc_url( home_url( '/colaboraciones/' ) ); ?>"} /-->
 				<!-- wp:navigation-link {"label":"<?php esc_attr_e( 'Contacto', 'lamutable' ); ?>","url":"<?php echo esc_url( home_url( '/contacto/' ) ); ?>"} /-->
+				<!-- wp:navigation-link {"label":"<?php esc_attr_e( 'Aviso legal', 'lamutable' ); ?>","url":"https://lamutable.es/aviso-legal/","kind":"custom"} /-->
+				<!-- wp:navigation-link {"label":"<?php esc_attr_e( 'Política de privacidad', 'lamutable' ); ?>","url":"https://lamutable.es/politica-de-privacidad/","kind":"custom"} /-->
+				<!-- wp:navigation-link {"label":"<?php esc_attr_e( 'Política de cookies', 'lamutable' ); ?>","url":"https://lamutable.es/politica-de-cookies/","kind":"custom"} /-->
 			<!-- /wp:navigation -->
 		</div>
 		<!-- /wp:group -->
